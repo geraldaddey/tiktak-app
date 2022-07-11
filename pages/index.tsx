afa0e2119d680da9b235442400f465f76bc28748
geraldaddey <geraldaddey@yahoo.com>
2022-07-11 13:36:01 +0000

    configured sanity. lost mine too

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,9 @@ const Home: NextPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        Nicely Setup. getting the bag real soon
+        Nicely Setup. getting the bag real soon. More on the way
       </h1>
+      <p> Finally fixed everyhting with Sanity. Lost my sanity too.</p>
     </div>
   );
 };
