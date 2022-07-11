0c70cd6cf1c99c273bb044e97476db5e07c9073e
geraldaddey <geraldaddey@yahoo.com>
2022-07-11 14:12:04 +0000

    assets inmported, styling components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link>
+        <div>
+          <Image></Image>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
 import type { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 const Home: NextPage = () => {
   return (
