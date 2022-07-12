9f536f30aefb4fdc3f44de0c19ee904acf40cde1
geraldaddey <geraldaddey@yahoo.com>
2022-07-12 10:47:00 +0000

    navbar styled, logo modifications

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,12 +9,20 @@ import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
+import Logo from "../utils/tiktak-logo-2.png";
+
 const Navbar = () => {
   return (
-    <div>
-      <Link>
-        <div>
-          <Image></Image>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="flex-row flex items-center last:border border-black h-8 w-8 bg-red-700">
+          <Image
+            className="cursor-pointer"
+            alt="TikTak"
+            src={Logo}
+            layout="responsive"
+          ></Image>
+          <p className="ml-10">TikTak</p>
         </div>
       </Link>
     </div>
