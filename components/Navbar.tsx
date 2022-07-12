37fd1b297b18b99dc75f475b3314f136ee2b2e78
geraldaddey <geraldaddey@yahoo.com>
2022-07-12 12:34:54 +0000

    styles sidebar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div className="flex-row flex items-center last:border border-black h-8 w-8 bg-red-700">
+        <div className="flex-row flex items-center last:border h-8 w-8">
           <Image
             className="cursor-pointer"
             alt="TikTak"
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,32 +9,45 @@ import { ImCancelCircle } from "react-icons/im";
 
 const Sidebar = () => {
   const [showSideBar, setShowSideBar] = useState(true);
+
+  const userProfile = false;
+
+  const normalLink =
+    "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[green] rounded";
   return (
     <div>
-      <div className="block xl:hidden m-2 ml-4 mt-3 text-xl"></div>
+      <div className="block xl:hidden m-2 ml-4 mt-3 text-xl">
+        {showSideBar ? (
+          <ImCancelCircle
+            onClick={() => {
+              setShowSideBar((prev) => !prev);
+            }}
+          />
+        ) : (
+          <AiOutlineMenu />
+        )}
+      </div>
 
-      {showSideBar ? (
-        <ImCancelCircle
-          onClick={() => {
-            setShowSideBar((prev) => !prev);
-          }}
-        />
-      ) : (
-        <AiOutlineMenu />
-      )}
       <div>
         {showSideBar && (
           <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
             <div className="xl:border-b-2 border-gray-200 xl:pb-4">
               <Link href="/">
-                <div>
+                <div className={normalLink}>
                   <p className="text-2xl">
                     <AiFillHome />
                   </p>
-                  <span>For You</span>
+                  <span className="text-xl hidden xl:block">For You</span>
                 </div>
               </Link>
             </div>
+            {!userProfile && (
+              <div className="px-2 py-4 hidden xl:block">
+                <p className="text-gray-400">
+                  Log in to like and comment on videos
+                </p>
+              </div>
+            )}
           </div>
         )}
       </div>
